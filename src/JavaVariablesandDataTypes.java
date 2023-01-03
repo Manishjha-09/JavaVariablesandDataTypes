dcfd676aad2007798c3c43d6b506cca8ae70213b
Manishjha-09 <121721616+Manishjha-09@users.noreply.github.com>
2023-01-03 20:03:27 +0530

    Update JavaVariablesandDataTypes.java

--- src/JavaVariablesandDataTypes.java
+++ src/JavaVariablesandDataTypes.java
@@ -1,26 +1,26 @@
 public class JavaVariablesandDataTypes {
     public static void main(String[] args) {
-        // First Question
+        // Q1. Take 2 integer values in two variables x and y and print their product.
         int x = 2;
         int y = 4;
         System.out.println(x*y);
 
-        // Second Question
+        // Q2. Print the ASCII value of character ‘U’.
         int ascii = 'U';
         System.out.println(ascii);
 
-        // Third Question
+        // Q3. Write a Java program to take the length and breadth of a rectangle and print its area.
         int length = 7;
         int breadth = 4;
         int area = length*breadth;
         System.out.println(area);
 
-        // Fourth Question
+        // Q4. Write a Java program to calculate the cube of a number.
         int n = 4;
         int cube = n*n*n;
         System.out.println(cube);
 
-        // Fifth Question
+        //  Q5. Write a Java program to swap two numbers with the help of a third variable.
         int a = 2;
         int b = 3;
         int c = a;
